8ccdcbf443ffb89be181954648bc85eb407ae1e5
Duong Dang Duy <ddduong@tma.com.vn>
2020-05-24 14:20:51 +0700

    aaaa

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const bodyParser = require('body-parser')
 
 const adapter = new FileSync('db.json')
 const db = low(adapter)
-var port = process.env.port || 3000;
+var port = process.env.PORT || 3000;
 server.listen(port);
 app.use(express.static('public'))
 app.use(bodyParser.json())
